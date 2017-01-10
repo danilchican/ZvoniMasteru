38d8bb75a9faafd9aceccf701c1d3450c90fdb88
danilchican <danilchican@mail.ru>
2017-01-11 00:20:23 +0300

    Created companies migration

---
+++ database/migrations/2017_01_11_000047_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name', 255);
+            $table->string('slug', 80);
+            $table->text('description');
+            $table->string('logo_url', 255);
+            $table->string('status')->default('none');  // approved, refused, moderate, none
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
