b51773e299df8544584f13382ca5e9921005932c
danilchican <danilchican@mail.ru>
2017-01-10 21:15:43 +0300

    Created PolicyTest

---
+++ tests/PolicyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\User;
+use App\Models\Policy;
+
+class PolicyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Test to create policy.
+     *
+     * @return void
+     */
+    public function testPolicyCanBeCreated()
+    {
+        $policy = factory(App\Models\Policy::class)->create([
+            'title' => 'Administrator',
+        ]);
+
+        $this->assertEquals($policy->title, 'Administrator');
+
+        $this->seeInDatabase('policies', ['id' => '1', 'title' => 'Administrator']);
+    }
+
+    public function testPolicyCanBeDeleted()
+    {
+        $policy = Policy::create(['title' => 'Administrator']);
+
+        $policy->delete();
+
+        $this->NotSeeInDatabase('policies', ['id' => '1', 'title' => 'Administrator']);
+    }
+
+    public function testPolicyCanBeAssignedToUser()
+    {
+        Policy::create(['title' => 'Administrator']);
+
+        $user = factory(User::class)->create([
+            'policy_id' => 1,
+        ]);
+
+        $this->assertEquals($user->policy_id, 1);
+
+        $founded_user = User::find(1);
+
+        $this->assertEquals($founded_user->policy_id, 1);
+    }
+}
