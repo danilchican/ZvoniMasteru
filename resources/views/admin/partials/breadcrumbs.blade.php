62a979252b0c48a36d87cebfced3c5b77d3fd737
danilchican <danilchican@mail.ru>
2017-01-11 12:07:23 +0300

    Fixed breadcrumbs.

--- resources/views/admin/partials/breadcrumbs.blade.php
+++ resources/views/admin/partials/breadcrumbs.blade.php
@@ -1,8 +1,10 @@
 @if ($breadcrumbs)
     <ul class="breadcrumb">
         @foreach ($breadcrumbs as $breadcrumb)
-            @if (!$breadcrumb->last)
+            @if ($breadcrumb->first)
                 <li><a href="{{ $breadcrumb->url }}"><i class="fa fa-dashboard"></i> {{ $breadcrumb->title }}</a></li>
+            @elseif(!$breadcrumb->first && !$breadcrumb->last)
+                <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
             @else
                 <li class="active">{{ $breadcrumb->title }}</li>
             @endif
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -6,7 +6,7 @@ Breadcrumbs::register('admin', function ($breadcrumbs) {
     $breadcrumbs->push('Dashboard');
 });
 
-// Admin > Companies
+// Admin > Dashboard > Companies
 Breadcrumbs::register('admin.companies.index', function ($breadcrumbs) {
     $breadcrumbs->parent('admin');
     $breadcrumbs->push('Companies', route('admin.companies.index'));
