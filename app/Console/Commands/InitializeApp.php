a9e5172894f340b5f3a6c13b18fc6d52d882cc5b
danilchican <danilchican@mail.ru>
2017-01-11 17:48:22 +0300

    Created command to init all components of app

---
+++ app/Console/Commands/InitializeApp.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InitializeApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialize all app components.';
+
+    /**
+     * Database seeder variable.
+     *
+     * @var \DatabaseSeeder
+     */
+    protected $seeder;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->seeder = new \DatabaseSeeder();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $this->info('Resetting all migrations..');
+        $this->call('migrate:reset');
+
+        $this->info('Start migrate..');
+        $this->call('migrate');
+
+        $this->info('Running seeders..');
+        $this->seeder->run();
+        $this->info('Seeders finished');
+    }
+}
