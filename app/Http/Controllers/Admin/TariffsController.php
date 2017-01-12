e53bef333ef6bcc96b93f26a5bfdf0d5498ae613
danilchican <danilchican@mail.ru>
2017-01-12 12:43:52 +0300

    Created base view and functionality to create() tariff

--- app/Http/Controllers/Admin/TariffsController.php
+++ app/Http/Controllers/Admin/TariffsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Service;
 use App\Models\Tariff;
+use App\Models\Price;
 use Illuminate\Http\Request;
 
 class TariffsController extends AdminController
@@ -41,13 +42,46 @@ class TariffsController extends AdminController
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $attributes = ['title', 'whom', 'additional_service', 'top', 'published'];
+
+        $prices = $request->input('prices');
+        $ranges = $request->input('ranges');
+        $services = (array)$request->input('services');
+
+        $services_objects = [];
+        $prices_objects = [];
+
+        if($services) {
+            foreach($services as $service) {
+                $service_obj = new Service();
+                $service_obj->setTitle($service);
+                $services_objects[] = $service_obj;
+            }
+        }
+
+        if(count($prices) === count($ranges)) {
+            for($i = 0, $max = count($prices); $i < $max; $i++) {
+                $price = new Price();
+                $price->setPrice($prices[$i]);
+                $price->setRange($ranges[$i]);
+
+                $prices_objects[] = $price;
+            }
+        }
+
+        $tariff = new Tariff($request->only($attributes));
+        $tariff->save();
+
+        $tariff->services()->sync($services);
+        $tariff->prices()->saveMany($prices_objects);
+
+        return redirect()->route('admin.tariffs.index')
+            ->with(['success' => 'Tariff successfully added']);
     }
 
     /**
@@ -88,14 +122,23 @@ class TariffsController extends AdminController
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the tariff from storage.
      *
-     * @param int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $tariff = Tariff::find($id);
+
+        if(!$tariff) {
+            return redirect()->back()
+                ->with(['error' => 'Tariff has not been found.']);
+        }
+
+        $tariff->delete();
+
+        return redirect()->back()
+            ->with(['success' => 'Tariff successfully deleted.']);
     }
 }
