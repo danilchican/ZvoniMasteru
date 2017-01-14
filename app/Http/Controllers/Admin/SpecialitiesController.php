72e7b08f3b14b41e5078552de3b37bb1b605fee5
danilchican <danilchican@mail.ru>
2017-01-14 19:23:45 +0300

    Deleted empty methods

--- app/Http/Controllers/Admin/SpecialitiesController.php
+++ app/Http/Controllers/Admin/SpecialitiesController.php
@@ -46,16 +46,6 @@ class SpecialitiesController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -78,28 +68,6 @@ class SpecialitiesController extends Controller
                 'Speciality created successfully.',
             ],
         ], 200);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
