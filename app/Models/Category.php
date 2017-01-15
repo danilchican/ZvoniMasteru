6b51cbb3afd25b8411a95b394d75756d63db4eb9
danilchican <danilchican@mail.ru>
2017-01-15 14:47:53 +0300

    Modified Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Category extends Model
 {
-    //
+    use NodeTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'desc',  'parent_id', '_lft', '_rgt',
+    ];
 }
