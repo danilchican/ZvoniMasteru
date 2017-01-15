bea5ad7196b2ba0c8f68ac59fc3a1a44b1ae6aea
danilchican <danilchican@mail.ru>
2017-01-15 14:37:56 +0300

    Created migration & model for categories

---
+++ database/migrations/2017_01_15_143528_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Kalnoy\Nestedset\NestedSet;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->text('desc')->nullable();
+            NestedSet::columns($table);
+            $table->timestamps();
+            $table->index(['_lft', '_rgt', 'parent_id', 'slug']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
