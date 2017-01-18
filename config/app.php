673a0f91d9b8a0e1a103a437b58ef0a35079d1e0
Danilchik Vladislav <danilchican@mail.ru>
2017-01-18 12:56:53 +0300

    Installed Image package.

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
         DaveJamesMiller\Breadcrumbs\ServiceProvider::class,
         Laravelrus\LocalizedCarbon\LocalizedCarbonServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -240,6 +241,7 @@ return [
         'DiffFormatter'     => Laravelrus\LocalizedCarbon\DiffFactoryFacade::class,
         'Form'              => Collective\Html\FormFacade::class,
         'Html'              => Collective\Html\HtmlFacade::class,
+        'Image'             => Intervention\Image\Facades\Image::class,
     ],
 
 ];
