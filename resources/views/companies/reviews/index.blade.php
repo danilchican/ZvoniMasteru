ab8d56c512c1f0aff2296ff5abcd865c4c4cc817
danilchican <danilchican@mail.ru>
2017-02-15 20:11:50 +0300

    Fixed view

--- resources/views/companies/reviews/index.blade.php
+++ resources/views/companies/reviews/index.blade.php
@@ -1,12 +1,15 @@
-@include('companies.reviews.create')
-
 @if(count($reviews) > 0)
-    <h3>Отзывы о компании:</h3>
-    @foreach($reviews as $review)
-        @include('companies.reviews.show', ['review' => $review])
-    @endforeach
+    @include('companies.reviews.create')
+    <div class="col-md-12">
+        <h3>Отзывы о компании:</h3>
+        @foreach($reviews as $review)
+            @include('companies.reviews.show', ['review' => $review])
+        @endforeach
+    </div>
 @else
     <div class="alert alert-info">Отзывов пока еще нет.
         <strong><a href="#create-review-form">Станьте первым!</a></strong>
     </div>
+
+    @include('companies.reviews.create')
 @endif
