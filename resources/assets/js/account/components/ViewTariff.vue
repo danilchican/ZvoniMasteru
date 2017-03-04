cff75edd3a10cd2c14676ce8df6fa2d4cf1a78dc
danilchican <danilchican@mail.ru>
2017-03-04 15:46:00 +0300

    Modified tariffs displaying

--- resources/assets/js/account/components/ViewTariff.vue
+++ resources/assets/js/account/components/ViewTariff.vue
@@ -1,23 +1,37 @@
 <template>
-    <li>
-        <div class="col-sm-6 col-md-6">
-            <div class="thumbnail" style="min-height: 475px;">
-                <div class="caption">
-                    <h3><strong>{{ tariff.title }}</strong></h3>
-                    <p><strong>ТОП:</strong> {{ tariff.top }}</p>
-                    <p><strong>Для кого:</strong> {{ tariff.whom }}</p>
-                    <p><strong>Доп. услуга:</strong> {{ tariff.additional_service }}</p>
-                    <p style="margin:0"><strong>Услуги:</strong></p>
-                    <ul v-for="service in tariff.services">
-                        <li>{{ service.title }}</li>
-                    </ul>
-                    <p style="margin:0"><strong>Цены:</strong></p>
-                    <ul v-for="price in tariff.prices">
-                        <li>{{ price.range }}: <strong>{{ price.price }}</strong></li>
-                    </ul>
-                    <br>
-                    <p><a href="#" class="btn btn-primary" role="button">Заказать</a></p>
-                </div>
+    <li class="col-sm-6 col-md-6">
+        <div class="panel panel-primary">
+            <!-- Default panel contents -->
+            <div class="panel-heading">{{ tariff.title }}</div>
+            <div class="panel-body">
+                <p><strong>ТОП:</strong> {{ tariff.top }}</p>
+                <p><strong>Для кого:</strong> {{ tariff.whom }}</p>
+                <p><strong>Доп. услуга:</strong> {{ tariff.additional_service }}</p>
+            </div>
+
+            <!-- List group -->
+            <ul v-for="service in tariff.services" class="list-group">
+                <li class="list-group-item">{{ service.title }}</li>
+            </ul>
+            <br>
+            <!-- Prices -->
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Период</th>
+                    <th>Цена</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="price in tariff.prices">
+                    <td>{{ price.range }}</td>
+                    <td>{{ price.price }}</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <div class="panel-body">
+                <p><a href="#" class="btn btn-primary" role="button">Заказать</a></p>
             </div>
         </div>
     </li>
