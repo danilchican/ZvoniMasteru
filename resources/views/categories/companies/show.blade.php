5dbe08d1bdf3eda32cb49bc4335ca0bba6612270
Danilchik Vladislav <danilchican@mail.ru>
2017-07-15 13:25:58 +0300

    Created present category page.

--- resources/views/categories/companies/show.blade.php
+++ resources/views/categories/companies/show.blade.php
@@ -1,27 +1,100 @@
-<div class="col-12 col-sm-12 col-lg-12 company-box">
-    <div class="thumbnail">
-        <div class="caption">
-            <div class="col-md-3">
-                <div class="row">
-                    <img class="featurette-image img-responsive" id="logo" alt="150x150" width="150" src="{{ $company->getLogo() }}">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 company-box">
+    <div class="row">
+        <div class="thumbnail">
+            <div class="caption">
+                <div class="col-xs-3 col-sm-3 col-md-4">
+                    <div class="row">
+                        <img class="featurette-image img-responsive company-logo" alt="220x220" width="220"
+                             src="{{ $company->getLogo() }}">
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="row">
-                    <h4><a href="{{ route('company.cart', $company->getSlug()) }}" target="_blank">{{ $company->getName() }}</a></h4>
-                    <p>{{ $company->getDescription() }}</p>
+                <div class="col-xs-9 col-sm-9 col-md-8">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                            <div class="row">
+                                <h4 class="company-box-title"><a href="{{ route('company.cart', $company->getSlug()) }}"
+                                                                 target="_blank">{{ $company->getName() }}</a></h4>
+                            </div>
+                            <div class="row">
+                                <p class="company-box-short-desc">{{ $company->getDescription() }}</p>
+                            </div>
+                            <div class="row">
+                                <div class="company-box-info">
+                                    <div class="col-md-4 col-sm-4 site-unp-box">
+                                        @if(!empty($website = $company->contacts->getWebsiteURL()))
+                                            <p class="company-box-website">
+                                                <a href="{{ $website }}">{{ $website }}</a>
+                                            </p>
+                                        @endif
+                                        <p class="company-box-unp">УНП: {{ $company->getUNPNumber() }}</p>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 company-box-socials">
+                                        <ul>
+                                            <li class="vkontakte"><a
+                                                        href="{{ $company->contacts->groups->getVkontakteURL() }}">Вконтакте</a>
+                                            </li>
+                                            <li class="facebook"><a
+                                                        href="{{ $company->contacts->groups->getFacebookURL() }}">Facebook</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 phones">
+                                        <div class="row">
+                                            <ul>
+                                                @foreach($company->contacts->phones()->filled()->get() as $phone)
+                                                    <li>{{ $phone->getNumber() }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row technical-info">
+                                <div class="col-md-4 col-sm-4 experience">
+                                    <p>Опыт работы более 3 лет</p>
+                                </div>
+                                <div class="col-md-4 col-sm-4 contract">
+                                    <p>Работа по договору</p>
+                                </div>
+                                <!--
+                                <div class="col-md-4 col-sm-4 rating">
+                                    <div class="row">
+                                        <img src="./assets/images/company/rating.jpg" style="margin-top: 7px" alt="Рейтинг">
+                                    </div>
+                                </div>
+                                -->
+                            </div>
+                            <div class="row contacts-info-mobile">
+                                <ul>
+                                    @if(($phone = $company->contacts->phones()->filled()->first()))
+                                        <li>{{ $phone->getNumber() }}</li>
+                                    @endif
+                                    @if(!empty($website))
+                                        <li><a href="{{ $website }}">{{ $website }}</a></li>
+                                    @endif
+                                    <li>УНП: {{ $company->getUNPNumber() }}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <p style="margin: 0">Телефоны:</p>
-                <ul style="padding-left: 20px">
-                    @foreach($company->contacts->phones()->filled()->get() as $phone)
-                        <li>{{ $phone->getNumber() }}</li>
-                    @endforeach
-                </ul>
-                <p style="margin: 0">Сайт:</p>
-                @if(!empty($website = $company->contacts->getWebsiteURL())) <a href="{{ $website }}">{{ $website }}</a>
-                @else Не заполнено @endif
+                <div class="col-md-12 technical-info-mobile">
+                    <div class="row">
+                        <div class="col-md-4 experience">
+                            <p>Опыт работы более 3 лет</p>
+                        </div>
+                        <div class="col-md-4 contract">
+                            <p>Работа по договору</p>
+                        </div>
+                        <!--
+                        <div class="col-md-4 rating">
+                            <div class="row">
+                                 <img src="./assets/images/company/rating.jpg" style="margin-top: 5px" alt="Рейтинг">
+                            </div>
+                        </div>
+                        -->
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,45 +4,58 @@
     <title>ZvoniMasteru.by</title>
 @endsection
 
-@section('styles')
-    <style>
-        .thumbnail .caption {
-            overflow: hidden;
-        }
-    </style>
-@endsection
-
 @section('content')
-    <div class="container companies">
-        <div class="row">
-            <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-                @foreach ($categories as $category)
-                    <div class="list-group" style="margin-bottom: 0;">
-                        <a href="{{ route('category.show', ['category' => $category->getSlug()]) }}" class="list-group-item">{{ $category->getName() }}</a>
+    <div id="category-header" style="background: url(/assets/images/categories/otdelka-bg.jpg) center top no-repeat">
+        <div class="container">
+            <h1 class="category-header-title">{{ $currentCategory->getName() }}</h1>
+            <ul class="category-header-steps">
+                <li class="first">Найдите лучшего для Вас специалиста</li>
+                <li class="second">Созвонитесь с ним</li>
+                <li class="third">Договоритесь</li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <p class="category-description">
+            В процессе создания уютного дома или обустройства квартиры неизменно приходится сталкиваться с ремонтом,
+            особенно если этого касается строительства нового дома или капитального ремонта.<br/>
+            Все виды отделочных работ. Большой выбор специалистов в этой области.
+        </p>
+        <div id="companies">
+            <div class="companies-nav-mobile">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target=".sidebar-offcanvas">
+                    <span class="sr-only">Toggle navigation</span>
+                    <div class="icon-bar-block">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
                     </div>
+                    <div class="icon-bar-block-name">Показать категории</div>
+                </button>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 collapse sidebar-offcanvas" id="sidebar"
+                 role="navigation">
+                @foreach ($categories as $category)
+                    <a href="{{ route('category.show', ['category' => $category->getSlug()]) }}"
+                       class="list-group-item">{{ $category->getName() }}</a>
                 @endforeach
             </div>
-            <div class="col-sm-9">
-            @if(count($companies) > 0)
-                <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8" id="companies-content">
+                @if(count($companies) > 0)
                     @foreach($companies as $company)
                         @include('categories.companies.show', ['company' => $company])
                     @endforeach
-                </div><!-- /.row -->
-
-                <div class="row">
-                    <div class="col-sm-6">
-                        {!! $companies->links() !!}
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            @else
-                <h4>Компаний в данной категории еще нет.</h4>
-            @endif
+                @else
+                    <h4>Компаний в данной категории еще нет.</h4>
+                @endif
+                <div class="clear"></div>
+                <div class="col-md-12" id="companies-pagination">
+                    {!! $companies->links('pagination.default') !!}
+                </div>
             </div>
         </div>
-    </div><!-- /.container -->
-@endsection
-
-@section('scripts')
-    <script src="/js/app.js"></script>
+    </div>
 @endsection
